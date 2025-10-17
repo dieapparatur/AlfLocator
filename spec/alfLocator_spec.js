19c02df60ce5a0bed1ddb65b7eae361b4533f823
Fynn <144113211+dieapparatur@users.noreply.github.com>
2025-10-17 13:11:53 +0200

    Failing test when string is just X

--- spec/alfLocator_spec.js
+++ spec/alfLocator_spec.js
@@ -8,6 +8,10 @@ describe('alfLocator', function() {
 	it("return spaceship lost forever with input of an empty string", function() {
 	  expect(locator.findSpaceship('')).toEqual("Spaceship lost forever");
 	});
-  
+	
+	it("return 0,0 when string is just 'X'", function() {
+		expect(locator.findSpaceship('X')).toEqual("[0,0]");
+	});
+
   });
   
--- src/alfLocator.js
+++ src/alfLocator.js
@@ -3,5 +3,8 @@ function alfLocator() {}
 const map = "..........\n..........\n..........\n.......X..\n..........\n..........\n";
 
 alfLocator.prototype.findSpaceship = function(map) {
-    
+    if (map === "") {
+      return "Spaceship lost forever";
+    }
+
 };
