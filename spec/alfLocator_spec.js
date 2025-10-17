1921fec602fa26e2191bcc13a20815787379d737
Fynn <144113211+dieapparatur@users.noreply.github.com>
2025-10-17 13:58:39 +0200

    Added tests

--- spec/alfLocator_spec.js
+++ spec/alfLocator_spec.js
@@ -1,17 +1,37 @@
-describe('alfLocator', function() {
+describe('alfLocator', function () {
 	let locator;
-  
-	beforeEach(function() {
-	  locator = new alfLocator();
+
+	beforeEach(function () {
+		locator = new alfLocator();
 	});
-  
-	it("return spaceship lost forever with input of an empty string", function() {
-	  expect(locator.findSpaceship('')).toEqual("Spaceship lost forever");
+
+	it("return spaceship lost forever with input of an empty string", function () {
+		expect(locator.findSpaceship('')).toEqual("Spaceship lost forever");
 	});
-	
-	it("return 0,0 when string is just 'X'", function() {
+
+	it("return 0,0 when string is just 'X'", function () {
 		expect(locator.findSpaceship('X')).toEqual("[0,0]");
 	});
 
-  });
-  
+	it("returns 'Spaceship lost forever.' when there is no X", function () {
+		expect(locator.findSpaceship('..........')).toEqual("Spaceship lost forever.");
+	});
+
+	it("matches the kata example [7, 2]", function () {
+		const map = "..........\n" +
+			"..........\n" +
+			"..........\n" +
+			".......X..\n" +
+			"..........\n" +
+			"..........";
+		expect(locator.findSpaceship(map)).toEqual([7, 2]);
+	});
+
+
+	it("returns lost when no X in multiple lines", function () {
+		const map = "..........\n" +
+			"..........\n" +
+			"..........";
+		expect(locator.findSpaceship(map)).toEqual("Spaceship lost forever.");
+	});
+});
--- src/alfLocator.js
+++ src/alfLocator.js
@@ -1,12 +1,27 @@
-function alfLocator() {}
+function alfLocator() { }
 
 const map = "..........\n..........\n..........\n.......X..\n..........\n..........\n";
 
-alfLocator.prototype.findSpaceship = function(map) {
-    if (map === "") {
-      return "Spaceship lost forever";
+alfLocator.prototype.findSpaceship = function (map) {
+  if (map === "") {
+    return "Spaceship lost forever.";
+  }
+
+  if (map === "X") {
+    return "[0,0]"
+  }
+
+  let rows = map.split("\n"); //converts the string into an array for each new line
+  let height = rows.length;
+
+  for (let i = 0; i < rows.length; i++) {
+    let row = rows[i];
+    let x = row.indexOf("X");
+
+    if (x !== -1) {
+      let y = height - 1 - i;
+      return [x,y];
     }
-    if (map === "X") {
-      return "[0,0]";
-    }
+  }
+
 };
